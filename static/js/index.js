d3c27e7dc8d676bed834540d37cf0f84cb4fbb4d
sasoripathos <sasori.pan.jerry@gmail.com>
2019-05-04 14:17:28 -0400

    Add explanation text

--- static/js/index.js
+++ static/js/index.js
@@ -18,6 +18,7 @@
         if(cnt == 3) {
             document.getElementById("inprogress").style.display="none";
             document.getElementById("analysis").disabled = false;
+            document.getElementById("explain").style.display="block";
         }
     }
 
@@ -35,6 +36,7 @@
             document.getElementById("analysis").disabled = true;
             document.getElementById("inprogress").style.display="block";
             document.getElementById('results').style.display = "none";
+            document.getElementById("explain").style.display="none";
             api.getAnalysis('result1');
             api.getAnalysis('result2');
             api.getAnalysis('result3');
