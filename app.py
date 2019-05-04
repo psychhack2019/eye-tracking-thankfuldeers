dce732d6cf042f42df316c8ceaf7e7a67814ec66
Logan Doyle <lkdoyle@mta.ca>
2019-05-03 21:10:02 -0400

    added Read command
    reading in the csv

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pandas
 import numpy as py
 
 # -----Global variables-----
-FILE_NAME = 'pupil_data.csv'
+FILE_NAME = 'pupil_data.csv.zip'
 
 # -----Code-----
+dat = pandas.read_csv("./" + FILE_NAME, low_memory=False)
+
+print(dat)
