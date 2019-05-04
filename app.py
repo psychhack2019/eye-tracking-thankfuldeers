8eaa80192b8734a1a5ae26370dc31d57dd73d253
Logan Doyle <lkdoyle@mta.ca>
2019-05-03 23:52:45 -0400

    cleaning of pupil diameter

--- app.py
+++ app.py
@@ -6,6 +6,30 @@ import numpy as py
 FILE_NAME = 'pupil_data.csv.zip'
 
 # -----Code-----
-dat = pandas.read_csv("./" + FILE_NAME, low_memory=False)
+def cleanData(data):
+    #Clean the blink trials
+    badindex = []
+    print("reading")
+    
+    for index, val in data['right_in_blink'].iteritems():
+        if val == 1:
+            print("test")
+            badindex.extend([index, index-1, index+1])
+            
+    print("dropping")
+    
+    dropped = data.drop(badindex)
 
-print(dat)
+    cleanData = dropped[dropped['right_in_saccade'] != 1]
+    
+    return cleanData
+    
+print("starting")
+dat = pandas.read_csv("./" + FILE_NAME, low_memory=False)
+print("done reading")
+print("beginning cleaning")
+cleaned = cleanData(dat)
+print("done cleaning")
+
+
+
